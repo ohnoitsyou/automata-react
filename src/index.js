150ad726fa9a4531ccdf435e3394a79d2adbf067
David Young <ohnoitsyou7043@gmail.com>
2016-05-01 01:05:18 -0400

    Making the RelayController visible

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-import { CommentBox } from './components/CommentBox'
 import LightController from './components/LightController'
+import RelayController from './components/RelayController'
 
 ReactDOM.render (
   <div>
     <LightController />
+    <RelayController />
   </div>,
   document.getElementById('react-mount')
 )
