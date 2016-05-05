8f14bdd58013fdd71362994889b1e7505887167b
Josh Gordon <github@joshgordon.net>
2016-05-04 20:48:37 -0400

    Ripping out immutable

--- src/components/LightController/LightMaster.jsx
+++ src/components/LightController/LightMaster.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import immutable from 'immutable'
 import $ from 'jquery'
 
 import Switch from './Switch'
@@ -7,29 +6,26 @@ import Switch from './Switch'
 class LightMaster extends React.Component {
   constructor (props) {
     super (props)
-    this.state = {states: immutable.fromJS({})}
+    this.state = {master: null, silent: null}
   }
 
   componentWillMount() {
     $.ajax('localhost:3001/api/light/state').then((state) => {
       console.log('Retreived state: %o', state)
-      this.setState({states: immutable.fromJS(state)})
+      this.setState({master: state.master, silent: state.silent});
     })
   }
 
   toggleMaster = () => {
     return () => {
       console.log('toggle-master')
-
-      let oldState = this.state.states.get('master');
-      this.setState({states: this.state.states.set('master', !oldState)});
+      this.setState({master: !this.state.master});
     }
   }
   toggleSilent = () => {
     return () => {
       console.log('toggle-silent')
-      let oldState = this.state.states.get('silent');
-      this.setState({states: this.state.states.set('silent', !oldState)});
+      this.setState({silent: !this.state.silent});
     }
   }
 
@@ -38,9 +34,9 @@ class LightMaster extends React.Component {
       <fieldset>
         <legend>Master</legend>
         <span>Power</span>
-        <Switch state={this.state.states.get('master')} onChange={this.toggleMaster()} />
+        <Switch state={this.state.master} onChange={this.toggleMaster()} />
         <span>Silent</span>
-        <Switch state={this.state.get('silent')} onChange={this.toggleSilent()} />
+        <Switch state={this.state.silent} onChange={this.toggleSilent()} />
       </fieldset>
     )
   }
